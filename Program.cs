d225d797f0701e7630b7ffd289c6bb83a34c692b
Sinqobile <sinqomas@gmail.com>
2025-03-29 09:57:38 +0200

    I have added the voice message file so that when the console opens, it greets the user before they start using the application

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace POE_st10203232
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //voice greeting
+            string audio = "welcome.wav";
+
+            using (var audioFile = new AudioFileReader(audio))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                // Wait until the sound finishes playing
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            Console.WriteLine("Playing audio...");
+            //image display as heading in ASCII
+            //ask user for name and personalise
+            //responsive and handles unexpected or invalid input
+            //enhance UI with visual elements
+            //github version control
+        }
+    }
+}
