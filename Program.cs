b4f289d8e5016e3430063d684f2441fec7ef089d
Sinqobile <sinqomas@gmail.com>
2025-03-31 22:15:21 +0200

    added ascii art that will display as a heading when the console starts running so it welcomes the user

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NAudio.Wave;
+using System.IO;
 
 namespace POE_st10203232
 {
@@ -28,8 +29,19 @@ namespace POE_st10203232
                 }
             }
 
-            Console.WriteLine("Playing audio...");
-            //image display as heading in ASCII
+
+            //ASCII art
+            string filePath = "ascii.txt";
+
+            if (File.Exists(filePath)) 
+            {
+                string heading = File.ReadAllText(filePath);
+                Console.WriteLine(heading);
+            }
+
+            Console.WriteLine("Welcome");
+
+            //image display as heading in ASCI
             //ask user for name and personalise
             //responsive and handles unexpected or invalid input
             //enhance UI with visual elements
